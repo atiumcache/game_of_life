c437e45e1213d21d975f0ee2faf895bb37af89b3
atiumcache <aattilio@pm.me>
2023-11-21 14:42:50 -0700

    added more seeds, cleaned up animatino

--- game_of_life.py
+++ game_of_life.py
@@ -1,11 +1,17 @@
 import numpy as np
 from blessed import Terminal
 from math import floor
+from time import sleep
 
 term = Terminal()
 
-universe = np.zeros((term.height,term.width))
+universe = np.zeros((term.height-1,term.width))
 
+num_generations = 500
+
+speed = 1
+
+"""Starting Seed Options"""
 beacon = [[1,1,0,0],
           [1,1,0,0],
           [0,0,1,1],
@@ -15,15 +21,28 @@ r_pentomino = [[0,1,1],
                [1,1,0],
                [0,1,0]]
 
+diehard = [[0,0,0,0,0,0,1,0],
+           [1,1,0,0,0,0,0,0],
+           [0,1,0,0,0,1,1,1]]
+
+acorn = [[0, 1, 0, 0, 0, 0, 0], 
+         [0, 0, 0, 1, 0, 0, 0], 
+         [1, 1, 0, 0, 1, 1, 1]]
+
+block_switch = [[0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 1, 1],
+                [0, 0, 0, 0, 1, 0, 1, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0],
+                [1, 0, 1, 0, 0, 0, 0, 0]]
+
 
 def set_initial_universe(chosen_seed):
     """
     Place the seed cells within the initial universe. 
 
     :param chosen_seed: the user's seed choice
-
     """
-    
     seed_start_y = floor(term.height/2) - floor(len(chosen_seed)/2)
     seed_start_x = floor(term.width/2) - floor(len(chosen_seed[0])/2)
     universe[seed_start_y:seed_start_y+len(chosen_seed), seed_start_x:seed_start_x+len(chosen_seed[0])] = chosen_seed
@@ -33,7 +52,11 @@ def survival(x, y, universe):
     """
     Determine survival or death of each cell. 
 
-
+    :param x: current x-coordinate
+    :param y: current y-coordinate
+    :param universe: current universe
+    :return: 0 for death, 1 for life
+    :rtype: int
     """
     num_neighbors = np.sum(universe[x - 1 : x + 2, y - 1 : y + 2]) - universe[x, y]
     if universe[x, y] and not 2 <= num_neighbors <=3:
@@ -62,65 +85,97 @@ def generation(some_universe):
 def refresh_screen():
     """
     Display a new generation.
-
-
     """
     with term.cbreak(), term.hidden_cursor():
-        print(term.fullscreen, term.black_on_olive + term.clear)
+        print(term.white_on_darkgreen(term.center('Begin Life: Spacebar      Quit: Ctrl+C')))
         for row in universe:
             for cell in row:
                 if cell == int(0):
-                    print(term.white_on_darkgreen('0'), end='')
+                    print(term.lightgreen_on_black(' '), end='')
                 else:
-                    print(term.white_on_darkgreen('1'), end='')
-        print('\n')
+                    print(term.lightgreen_on_black('\u2593'), end='')
 
 
 def options_sequence():
     """
     A sequence of input screens that allows the user to customize the Game of Life. 
-
-
     """
-    print(term.fullscreen, term.clear + term.move_y(term.height //2))
-    print(term.white_on_darkgreen(term.center('Conway\'s Game of Life.')))
-    print(term.black_on_darkgreen(term.center('Choose your starting seed.')))
-    print(term.black_on_darkgreen(term.center('1    Beacon')))
-    print(term.black_on_darkgreen(term.center('2    R-Pentomino')))
+    print(term.clear + term.move_y(term.height //2))
+    print(term.bold_white_on_darkgreen(term.center('Conway\'s Game of Life')))
+    print(term.white_on_darkgreen(term.center('Choose your starting seed.')))
+    print(term.white_on_darkgreen(term.center('1    Beacon             ')))
+    print(term.white_on_darkgreen(term.center('2    R-Pentomino        ')))
+    print(term.white_on_darkgreen(term.center('3    Diehard            ')))
+    print(term.white_on_darkgreen(term.center('4    Acorn              ')))
+    print(term.white_on_darkgreen(term.center('5    Block Switch Engine')))
+
     with term.hidden_cursor(), term.cbreak():
-        input = ''
-        while input == '':
-            input = term.inkey()
-            if input == '1':
+        inp = ''
+        while inp == '':
+            inp = term.inkey()
+            if inp == '1':
                 set_initial_universe(beacon)
-            elif input == '2':
+            elif inp == '2':
                 set_initial_universe(r_pentomino)
+            elif inp == '3':
+                set_initial_universe(diehard)
+            elif inp == '4':
+                set_initial_universe(acorn)
+            elif inp == '5':
+                set_initial_universe(block_switch)
             else:
                 break
 
-        
+    print(term.clear + term.move_y(term.height //2))
+    print(term.bold_white_on_darkgreen(term.center('Conway\'s Game of Life')))
+    print(term.white_on_darkgreen(term.center('Choose your speed.')))
+    print(term.white_on_darkgreen(term.center('1    Slow  ')))
+    print(term.white_on_darkgreen(term.center('2    Medium')))
+    print(term.white_on_darkgreen(term.center('3    Fast  ')))
+
+    with term.hidden_cursor(), term.cbreak():
+        global speed
+        inp = ''
+        while inp == '':
+            inp = term.inkey()
+            if inp == '1':
+                speed = 0.5
+            elif inp == '2':
+                speed = 0.07
+            elif inp == '3':
+                speed = 0.01
+            else:
+                break
 
 
 def main():
     global universe
 
-    print(term.fullscreen, term.clear + term.move_y(term.height //2))
-    print(term.white_on_darkgreen(term.center('Conway\'s Game of Life.')))
-    print(term.black_on_darkgreen(term.center('Press any key to continue.')))
-    print(term.black_on_darkgreen(term.center('Ctrl + C to quit.')))
+    print(term.clear + term.move_y(term.height //2))
+    print(term.bold_white_on_darkgreen(term.center('Conway\'s Game of Life')))
+    print(term.white_on_darkgreen(term.center('Press any key to continue.')))
+    print(term.white_on_darkgreen(term.center('Ctrl+C to quit.')))
 
     with term.hidden_cursor(), term.cbreak():
-        input = ''
-        while input == '':
-            input = term.inkey()
+        inp = ''
+        while inp == '':
+            inp = term.inkey()
         options_sequence()
 
     with term.hidden_cursor(), term.cbreak():
-        input = ''
-        while input.lower != 'q':
-            input = term.inkey(timeout=3)
-            if input == 'a':
-                universe = generation(universe)
-                refresh_screen()
+        refresh_screen()
+        inp = ''
+        while inp != ' ':
+            with term.hidden_cursor(), term.cbreak():
+                inp = term.inkey()
+        for i in range(num_generations):
+            universe = generation(universe)
+            print(term.clear)
+            refresh_screen()
+            sleep(speed)
+            # If window resizes, we break the game. 
+            if term.width != len(universe[0]):
+                break
 
-main()
+with term.fullscreen():
+    main()
